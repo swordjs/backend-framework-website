f99af2e76316d44e89fd4c16c058a8322ead8f65
seho <1018715564@qq.com>
2022-03-20 00:20:37 +0800

    🐞 fix: 修复CI资源 404的问题

--- next.config.js
+++ next.config.js
@@ -1,7 +1,32 @@
 /** @type {import('next').NextConfig} */
+
+const isProd = process.env.NODE_ENV === "production";
+
 const nextConfig = {
   reactStrictMode: true,
-  assetPrefix: "/backend-framework-website"
+  assetPrefix: getBasePath(),
+  basePath: getBasePath(),
+  webpack(webpackConfig) {
+    webpackConfig.output.publicPath =
+      getBasePath() + webpackConfig.output.publicPath; //资源生成前缀
+    return webpackConfig;
+  },
+
+  publicRuntimeConfig: {
+    basePath: getBasePath(), //写入路径
+  },
+};
+
+function getBasePath() {
+  var basePath = "";
+  if (isProd && process.env.BASE_PATH) {
+    if (process.env.BASE_PATH.startsWith("/")) {
+      basePath = process.env.BASE_PATH;
+    } else {
+      basePath = "/" + process.env.BASE_PATH;
+    }
+  }
+  return basePath;
 }
 
-module.exports = nextConfig
+module.exports = nextConfig;

cd93a7ae844368dfe00d4f3ca7a8776e477e2cd7
1018715564 <seho.zaq@gmail.com>
2022-04-10 12:34:20 +0000

    Deploying to gh-pages from  @ 63cf469b60cbbda77e33f153e7e06b2e66d25035 🚀

--- _next/static/njzPqGXj_mzS5yXKGW4Hx/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/286-0576cc7b575fb694.js","static/chunks/pages/index-6a4b426db0bad3d6.js"],"/_error":["static/chunks/pages/_error-2280fa386d040b66.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
